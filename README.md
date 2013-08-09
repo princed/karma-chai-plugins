0db20f8a15549013b2c9178eaef28a33ed79e338
Eugene Datsky <eugene@datsky.ru>
2013-08-09 13:20:37 +0400

    * chai is now included * karma as peer dep * limited requirejs support * version bump to 0.10.0

--- README.md
+++ README.md
@@ -3,22 +3,6 @@ karma-chai-plugins
 
 > [Chai](http://chaijs.com) browser plugins set for [Karma](http://karma-runner.github.io)
 
-
-Requirements
-------------
-
-This module currently requires the `canary` version of Karma:
-
-```sh
-$ npm install 'karma@canary' --save-dev
-```
-
-Install the `karma-chai` from Github:
-
-```sh
-$ npm install 'git+https://github.com/xdissent/karma-chai.git' --save-dev
-```
-
 Installation
 ------------
 
@@ -43,15 +27,27 @@ module.exports = (karma) ->
 Bundled plugins
 ---------------
 
+* [chai](http://chaijs.com)
 * [chai-as-promised](http://chaijs.com/plugins/chai-as-promised)
 * [chai-jquery](http://chaijs.com/plugins/chai-jquery) (jQuery should be included manually)
 * [sinon-chai](http://chaijs.com/plugins/sinon-chai) (sinon will be included automatically)
+
+Limited require.js support
+--------------------------
+
+`karma-chai-plugins` supports `requirejs` in tests, but for now in should be stated in frameworks keys before `chai` and other plugins.
+
+Correct usage:
+```coffee
+frameworks: ['requirejs', 'chai', 'chai-as-promised']
+```
 
 Additional features
 -------------------
 
 `chai-as-promised` plugin also provides global `expectDeferred` function that allows to test ‘jQuery-style promises’ as other promsises implementation.
 
+Usage:
 ```coffee
 expectDeferred(somePromise).to.become(someValue)
 ```
--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 var path = require('path');
 
-var pattern = function(file) {
-    return {pattern: file, included: true, served: true, watched: false};
+// TODO Do not expect require to be included before plugins
+var requireUsed = function(files) {
+    if (files) {
+        for (var i = files.length, pattern; i > 0; i--) {
+            pattern = files[i] && files[i].pattern;
+
+            if (typeof pattern === 'string' && pattern.indexOf('/karma-requirejs/') !== -1) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+};
+
+var pattern = function(file, files) {
+    return {pattern: file, included: !requireUsed(files), served: true, watched: false};
 };
 
 var plugins = {
+    'chai': function(files) {
+        if(!requireUsed(files)) {
+            files.unshift(pattern(path.join(__dirname, 'chai-adapter.js')));
+        }
+        files.unshift(pattern(path.resolve(require.resolve('chai'), '../chai.js'), files));
+    },
     'chai-as-promised': function(files) {
         files.unshift(pattern(path.join(__dirname, 'expect-deferred.js')));
-        files.push(pattern(require.resolve('chai-as-promised')));
+        files.push(pattern(require.resolve('chai-as-promised'), files));
     },
     'sinon-chai': function(files) {
         files.unshift(pattern(path.resolve(require.resolve('sinon'), '../../pkg/sinon.js')));
-        files.push(pattern(require.resolve('sinon-chai')));
+        files.push(pattern(require.resolve('sinon-chai'), files));
     },
     'chai-jquery': function(files) {
-        files.push(pattern(require.resolve('chai-jquery')));
+        files.push(pattern(require.resolve('chai-jquery'), files));
     }
 };
 
