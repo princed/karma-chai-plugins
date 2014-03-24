bb30a1e0f458ddbcbd27f27f0eafdb9ad8a01ee3
Oliver Morgan <ollym@me.com>
2014-03-24 16:43:52 +0000

    typo

--- function-bind-polyfill.js
+++ function-bind-polyfill.js
@@ -3,14 +3,14 @@ Function.prototype.bind = function bind(that) { // .length is 1
     if (typeof target != "function") {
         throw new TypeError("Function.prototype.bind called on incompatible " + target);
     }
-    var args = slice.call(arguments, 1); // for normal call
+    var args = Array.prototype.slice.call(arguments, 1); // for normal call
     var bound = function () {
 
         if (this instanceof bound) {
 
             var result = target.apply(
                 this,
-                args.concat(slice.call(arguments))
+                args.concat(Array.prototype.slice.call(arguments))
             );
             if (Object(result) === result) {
                 return result;
@@ -20,7 +20,7 @@ Function.prototype.bind = function bind(that) { // .length is 1
         } else {
             return target.apply(
                 that,
-                args.concat(slice.call(arguments))
+                args.concat(Array.prototype.slice.call(arguments))
             );
 
         }
