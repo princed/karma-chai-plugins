5848ca7739e76ee69fea29a4ebe8b5743b2e6075
Oliver Morgan <ollym@me.com>
2014-04-09 13:37:29 +0100

    Update function-bind-polyfill.js

--- function-bind-polyfill.js
+++ function-bind-polyfill.js
@@ -1,35 +1,37 @@
-Function.prototype.bind = function bind(that) { // .length is 1
-    var target = this;
-    if (typeof target != "function") {
-        throw new TypeError("Function.prototype.bind called on incompatible " + target);
-    }
-    var args = Array.prototype.slice.call(arguments, 1); // for normal call
-    var bound = function () {
-
-        if (this instanceof bound) {
-
-            var result = target.apply(
-                this,
-                args.concat(Array.prototype.slice.call(arguments))
-            );
-            if (Object(result) === result) {
-                return result;
-            }
-            return this;
-
-        } else {
-            return target.apply(
-                that,
-                args.concat(Array.prototype.slice.call(arguments))
-            );
-
+if ( ! Function.prototype.bind) {
+    Function.prototype.bind = function bind(that) { // .length is 1
+        var target = this;
+        if (typeof target != "function") {
+            throw new TypeError("Function.prototype.bind called on incompatible " + target);
         }
-
+        var args = Array.prototype.slice.call(arguments, 1); // for normal call
+        var bound = function () {
+    
+            if (this instanceof bound) {
+    
+                var result = target.apply(
+                    this,
+                    args.concat(Array.prototype.slice.call(arguments))
+                );
+                if (Object(result) === result) {
+                    return result;
+                }
+                return this;
+    
+            } else {
+                return target.apply(
+                    that,
+                    args.concat(Array.prototype.slice.call(arguments))
+                );
+    
+            }
+    
+        };
+        if(target.prototype) {
+            empty = function() { }
+            empty.prototype = target.prototype;
+            bound.prototype = new empty();
+        }
+        return bound;
     };
-    if(target.prototype) {
-        empty = function() { }
-        empty.prototype = target.prototype;
-        bound.prototype = new empty();
-    }
-    return bound;
-};
+}
